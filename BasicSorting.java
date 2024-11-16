8975e3b235bae078ca9e5763d95b6db018ff1492
Anuja <agrawalanuja68@gmail.com>
2024-11-17 02:47:32 +0530

    changes made

--- BasicSorting.java
+++ BasicSorting.java
@@ -1,7 +1,12 @@
+// import java.util.Arrays;
+// import java.util.Collections;
 public class BasicSorting {
+
     public static void bubbleSort(int arr[]){
+        //outer loop running for number of turns....
         for(int turn=0; turn<arr.length-1; turn++){
            
+            // inner loop running for bubble sort....
             for(int j=0; j<arr.length-1-turn; j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
@@ -12,16 +17,14 @@ public class BasicSorting {
             }
         }
     }
-    public static void PrintArr(int arr[]){
-        for(int i=0; i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
-    }
+  
+    
     public static void selectionSort(int arr[]){
 
+        // outer loop runs one less than arr length....
         for(int i=0; i<arr.length-1; i++){
             int minpos = i;
+            // inner loop finding and placing the current value into minposition 
             for(int j=i+1; j<arr.length; j++){
                 if(arr[minpos] > arr[j]){
                     minpos = j;
@@ -34,18 +37,58 @@ public class BasicSorting {
         }
     }
 
+    public static void insertionSort(Integer arr[]){
+        
+        for(int i=1; i<arr.length; i++){
+            int curr = arr[i];
+            int prev = i-1;
+            // finding out the current position to insert....
+            while( prev >= 0 && arr[prev] > curr){
+                arr[prev +1] = arr[prev];
+                prev--;
 
-
-    public static void main(String argsString[]){
-        int arr[] = {5,4,1,3,2};
-        selectionSort(arr);
-        PrintArr(arr);
-
+            }
+            // performing inserton.....
+            arr[prev + 1] = curr;
+        }
+    }
+    public static void PrintArr(int arr[]){
+        for(int i=0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void countingSort(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        //outer loop....
+        for(int i=0; i<arr.length; i++){
+            largest = Math.max(largest, arr[i]);
+        }
+        //for finding frequency....
+        int count[] = new int[largest+1];
+        for(int i=0; i<arr.length;i++){
+            count[arr[i]]++;
+        }
+        //sorting....
+        int j=0;
+        for(int i=0; i<count.length;i++){
+            while(count[i] > 0){
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
     }
 
 
 
+    public static void main(String argsString[]){
+    int arr[] = {1,4,1,3,2,4,3,7,8,6,6,};
+       // insertionSort(arr);
+      // Arrays.sort(arr, 0, 3);
+      countingSort(arr);
+    //    Arrays.sort(arr, 0,4, Collections.reverseOrder());
+        PrintArr(arr);
 
-
-    
+    }
 }
